4f61772cd1deace4f974ffb488d83e3545fc6df6
lsymul <lsymul@stanford.edu>
2023-04-18 14:10:58 +0200

    added "plot_varX_varY" function
    Not perfect (dealing with the colors, etc)

--- R/plot_varX_varY.R
+++ R/plot_varX_varY.R
@@ -1,20 +1,75 @@
 
-plot_varX_varY <- function(inputs, Y, varX, varY, y_lab) {
-  input_var <- get_block_data(inputs)
-  blockX <- input_var$block[input_var$variable == varX]
-  df <- 
-    tibble(
-      x = inputs[[blockX]][,varX],
-      y = Y[,varY],
-      Arm = ifelse(inputs[["Arm"]]$Placebo, "Placebo","LACTIN-V")
-    ) 
-  
-  
-  ggplot(df, aes(x = x, y = y, col = Arm)) +
+#' Scatter plot of two variables against each other
+#'
+#' @param Xs (optional) the multiblock input (list of `matrices`/`data.frames`)
+#' @param Y (optional) the output `matrix`/`data.frame`
+#' @param res (optional - either `res` or `Xs` and `Y`) the result of `ade4::mbpls` or `ade4::mbpcaiv`
+#' @param varX a character specifying the x-axis variable (can be an explanatory or response variable)
+#' @param varY a character specifying the y-axis variable (can be an explanatory or response variable)
+#' @param color_by (optional) a character or a vector of the same length of the data
+#' specifying how the samples should be colored
+#'
+#' @return a `ggplot2` object
+#' @export
+#'
+plot_varX_varY <- function(Xs = NULL, Y = NULL, res = NULL, varX, varY, color_by = NULL) {
+
+  if (!is.null(res)) {
+    Y <- res$tabY
+    XY <- dplyr::bind_cols(res$tabX, Y)
+    all_vars <- blocks_and_variables(res) %>% add_Y_vars_to_blocks_and_variables(., Y)
+  } else {
+    if (is.null(Xs) | is.null(Y)) stop("Xs and Y or res must be provided.\n")
+    XY <- dplyr::bind_cols(blocks_to_df(Xs), Y)
+    all_vars <- .blocks_and_variables_from_list(Xs) %>% add_Y_vars_to_blocks_and_variables(., Y)
+  }
+
+  if (!is.null(color_by)) {
+    if (is.character(color_by) & (length(color_by) == 1) & (color_by %in% all_vars$variable)) {
+      colors <- XY[,color_by]
+    } else if (length(color_by) == nrow(XY)) {
+      colors <- color_by
+      } else
+        stop("color_by must be a character providing the name of an X variable or
+                 a value vector with the same length as the dimension of the data.\n")
+  } else {
+    colors <- "A"
+  }
+
+
+  df <- tibble(x = XY[,varX], y = XY[,varY], color = colors)
+
+ g <-
+   ggplot(df, aes(x = x, y = y, col = color)) +
     geom_point(alpha = 0.5, size = 0.75) +
-    geom_smooth(method = "lm", formula = "y ~ x", aes(fill = Arm)) +
-    scale_color_manual(values = c("steelblue2","coral")) +
-    scale_fill_manual(values = c("steelblue2","coral")) +
-    xlab(varX) + ylab(y_lab)
-  
+    geom_smooth(method = "lm", formula = "y ~ x", aes(fill = color)) +
+    xlab(varX) + ylab(varY)
+
+  if (all(colors == "A")) {
+    g <-
+      g + guides(col = "none", fill = "none") +
+      scale_color_manual(values = "black") +
+      scale_fill_manual(values = "gray50")
+  }
+
+ g
+}
+
+add_Y_vars_to_blocks_and_variables <- function(input_vars, Y) {
+  all_vars <-
+    input_vars %>%
+    dplyr::mutate(
+      block = block %>% forcats::fct_expand("Y"),
+      pretty_block = pretty_block %>% forcats::fct_expand("Y"),
+      variable = variable %>% forcats::fct_expand(colnames(Y))
+    )
+  all_vars <-
+    all_vars %>%
+    dplyr::bind_rows(
+      dplyr::tibble(
+        block = "Y" %>% factor(., levels = levels(all_vars$block)),
+        variable = colnames(Y) %>% factor(., levels = levels(all_vars$variable)),
+        pretty_block = "Y" %>% factor(., levels = levels(all_vars$pretty_block))
+        )
+    )
 }
