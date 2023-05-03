a7e7a0447e3df859c74b86b7d2dfff67697f9f56
lsymul <lsymul@stanford.edu>
2023-05-03 09:44:20 -0700

    exporting the MB PLS DA functions

--- R/plot_mtbda_coef.R
+++ R/plot_mtbda_coef.R
@@ -1,4 +1,21 @@
 
+#' Plots the coefficients of associations between the explanatory and response variables
+#'
+#' @param res the output of `ade4::mbpls` (or `ade4::mbpcaiv`) (the input of `ade4::randboot`)
+#' @param boot (optional) the output of `ade4::randboot` to display the confidence intervals
+#' @param Y_var (optional) a `integer` or a `character` specifying the indices
+#' or names of selected response variables if one desires to display the
+#' coefficients for a subset of variables.
+#' @param format "long" or "wide". a `character` specifying if the coefficients should be displayed horizontally or vertically. (default = "long")
+#'
+#' @return a `ggplot2` object
+#' @export
+#' @import ggplot2
+#' @importFrom dplyr tibble as_tibble mutate row_number group_by summarize left_join join_by arrange n
+#' @importFrom tidyr pivot_longer
+#' @importFrom stringr str_wrap
+#' @importFrom stats quantile
+#' @importFrom forcats fct_rev
 plot_mtbda_coef <- function(res, boot = NULL, Yvar, format = "long") {
 
   coefs <- get_mtbda_coef(res = res, boot = boot)
